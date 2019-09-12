2fb53fd1dc5c27c877ed11ec8307a17c7181c647
MarlonIC <marlonricslim@gmail.com>
2019-09-12 14:48:07 -0500

    feat(exercises): resolve exercises of operations basic

---
+++ 2_9_number_favorite.py
@@ -0,0 +1,3 @@
+favorite_number = 16
+
+print(f"Your favorite number is {favorite_number}")
--- __init__.py
+++ __init__.py
@@ -4,3 +4,11 @@ last_name = "Inga Cahuana"
 full_name = f"{first} {last_name}"
 
 print(full_name)
+
+
+print(3 / 2)
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+universe_age = 14_000_000_000
+print(universe_age)
